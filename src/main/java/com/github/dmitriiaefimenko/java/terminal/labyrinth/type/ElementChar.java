3a80a63de49ba7977f1c360d353a249c3b904af2
Dmitrii Efimenko <dmitriiaefimenko@gmail.com>
2024-05-03 23:21:49 +0300

    Added GUI

---
+++ src/main/java/com/github/dmitriiaefimenko/java/terminal/labyrinth/type/ElementChar.java
@@ -0,0 +1,11 @@
+package com.github.dmitriiaefimenko.java.terminal.labyrinth.type;
+
+public interface ElementChar {
+
+    char USER   = 'U';
+    char HUNTER = 'H';
+    char CARD   = 'C';
+    char WALL   = '#';
+    char EXIT   = 'E';
+    char EMPTY  = ' ';
+}
